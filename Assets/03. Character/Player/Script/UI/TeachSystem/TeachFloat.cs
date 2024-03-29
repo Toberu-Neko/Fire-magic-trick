973bf2cc6e39972ecb88c230d195216735157a61
bottledsnow <snowmelt945@gmail.com>
2024-03-29 11:28:54 +0800

    補

--- Assets/03. Character/Player/Script/UI/TeachSystem/TeachFloat.cs
+++ Assets/03. Character/Player/Script/UI/TeachSystem/TeachFloat.cs
@@ -1,40 +1,36 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TeachFloat : MonoBehaviour
 {
     [SerializeField] private GameObject UI_SuperJump;
     [SerializeField] private GameObject DashState;
+    [SerializeField] private GameObject JumpWeakest;
 
     private void Start()
     {
         UI_SuperJump.SetActive(false);
         DashState.SetActive(false);
+        JumpWeakest.SetActive(false);
     }
     public enum types
     {
         SuperJump,
         DashState,
+        JumpWeakest,
         FireSkill,
         WindSkill
     }
     public void Open(types type)
     {
-        GameObject obj = null;
-
-        switch (type)
-        {
-            case types.SuperJump:
-                obj = UI_SuperJump;
-                break;
-            case types.DashState:
-                obj = DashState;
-                break;
-        }
-
+        GameObject obj = ChooseObj(type);
         obj.SetActive(true);
     }
     public void Close(types type)
+    {
+        GameObject obj = ChooseObj(type);
+        obj.SetActive(false);
+    }
+    private GameObject ChooseObj(types type)
     {
         GameObject obj = null;
 
@@ -46,8 +42,11 @@ public class TeachFloat : MonoBehaviour
             case types.DashState:
                 obj = DashState;
                 break;
+            case types.JumpWeakest:
+                obj = JumpWeakest;
+                break;
         }
 
-        obj.SetActive(false);
+        return obj;
     }
 }
