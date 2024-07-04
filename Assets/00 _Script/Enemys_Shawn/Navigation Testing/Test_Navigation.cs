74b6c66e90871f22a545e1eab83f6ae794e9355e
bottledsnow <snowmelt945@gmail.com>
2024-07-04 23:03:53 +0800

    Navigation

---
+++ Assets/00 _Script/Enemys_Shawn/Navigation Testing/Test_Navigation.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Test_Navigation : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
